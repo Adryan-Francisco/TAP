8f4cc4c98ebb4fb2f6b018db940fd2b8e31e8e18
Adryan-Francisco <adryanfrancisco62@gmail.com>
2025-06-03 15:44:52 -0300

    03-06

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,56 +1,71 @@
-import { useState } from 'react';
-import styles from './Login.module.css';
-
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (email && password) {
-      console.log('Login realizado com:', email, password);
-    } else {
-      setError('Por favor, preencha todos os campos!');
+import { useState, useEffect } from 'react';
+  import styles from './Login.module.css';
+  import { useAuthentication } from '../hooks/useAuthentication';
+ 
+  const Login = () => {
+    const [displayEmail, setEmail] = useState('');
+    const [displayPassword, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const { login, error: authError, loading } = useAuthentication();
+ 
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+ 
+      setError("");
+ 
+      const user = {
+        email: displayEmail,
+        password: displayPassword
+      }
+ 
+      const res = await login(user);
+ 
     }
+ 
+    useEffect(() => {
+      if (authError) {
+        setError(authError);
+      }
+    }, [authError]);
+ 
+ 
+    return (
+      <div className={styles.container}>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>E-mail:</span>
+            <input
+              type="email"
+              name="displayEmail"
+              required
+              placeholder="Digite seu E-mail"
+              value={displayEmail}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+ 
+          <label>
+            <span>Senha:</span>
+            <input
+              type="password"
+              name="displayPassword"
+              required
+              placeholder="Digite sua Senha"
+              value={displayPassword}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+ 
+          {error && <p className={styles.error}>{error}</p>}
+ 
+          <button type="submit" className={styles.btnLogin}>
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   };
-
-  return (
-    <div className={styles.container}>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>E-mail:</span>
-          <input
-            type="email"
-            name="email"
-            required
-            placeholder="Digite seu E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        
-        <label>
-          <span>Senha:</span>
-          <input
-            type="password"
-            name="password"
-            required
-            placeholder="Digite sua Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-
-        {error && <p className={styles.error}>{error}</p>}
-
-        <button type="submit" className={styles.btnLogin}>
-          Entrar
-        </button>
-      </form>
-    </div>
-  );
-};
-
-export default Login;
+ 
+  export default Login;
+ 
